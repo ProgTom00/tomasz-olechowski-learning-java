bd00ebf02a9e84448fbd0dcfa69cd93c989e1ba2
Tomasz Olechowski <tonaszolechowski1992@gmail.com>
2021-11-29 13:02:10 +0100

    Added test calculator

--- learning-testing/src/main/java/com/learning/testing/TestingMain.java
+++ learning-testing/src/main/java/com/learning/testing/TestingMain.java
@@ -1,7 +1,23 @@
 package com.learning.testing;
 
+import com.learning.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Check");
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.add(10, 5);
+        int subtractResult = calculator.subtract(10, 3);
+
+        if (addResult == 15) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error");
+        }
+        if (subtractResult == 7) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
