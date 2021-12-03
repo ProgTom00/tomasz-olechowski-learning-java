f043ba8a11e203a13d46aff8e2098bc104a59454
Tomasz Olechowski <tonaszolechowski1992@gmail.com>
2021-12-03 08:31:42 +0100

    Stream explanation

--- learning-stream/src/main/java/com/learning/stream/beautifier/PoemBeautifier.java
+++ learning-stream/src/main/java/com/learning/stream/beautifier/PoemBeautifier.java
@@ -4,7 +4,7 @@ public class PoemBeautifier {
     public void beautify(String sampleText, PoemDecorator poemDecorator) {
         String sampleResult = poemDecorator.decorate(sampleText);
         System.out.println("Text before using method: " + sampleResult);
-        System.out.println("Text after using methond: " + sampleResult);
+        System.out.println("Text after using method: " + sampleResult);
 
 
     }
